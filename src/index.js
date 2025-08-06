c0f173c85a4e1ed143dbde6340b0ab9696b705c4
fatima1414 <shaikhfatima5260@gmail.com>
2025-08-06 14:23:42 +0530

    code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import a from './App1.js'; 
+// import {a,b} from './App1.js'; 
 import reportWebVitals from './reportWebVitals';
+// import Add  from './App1';
+import App from './App1';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <h1> {a}</h1>    for single variable */}
+    {/* <h1>addition {a+b}</h1>  for multiple variable     */}
+    {/* <Add />  */}
+    {/* <App /> */}
+    <App></App>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
