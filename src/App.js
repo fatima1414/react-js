b1b365d4fcc5a54ab46715f6070c4721596af955
fatima1414 <shaikhfatima5260@gmail.com>
2025-08-06 10:32:41 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img
+          src="https://t3.ftcdn.net/jpg/09/36/32/36/240_F_936323620_mpIqx9HOhY8iH2UsojlNRf0C2UaWn3rp.jpg"
+          class="img-fluid rounded-top"
+          alt=""
+        />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          "To ride a horse is to fly without wings."
         </p>
         <a
           className="App-link"
@@ -15,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         show more
         </a>
       </header>
     </div>
